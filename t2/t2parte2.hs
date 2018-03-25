3142a65957e87fd21f80557b03b1079f3601c959
pprobst <pprobst@insiberia.net>
2018-03-25 09:10:59 -0300

    #3 e #4

--- t2/t2parte2.hs
+++ t2/t2parte2.hs
@@ -15,4 +15,20 @@ isBin str
 -- Mesmo que acima; não recursiva
 isBin' :: String -> Bool
 isBin' str = all (\x -> x `elem` "01") str
-  
+
+-- #3
+-- Implementar auxBin2Dec de forma recursiva, para que bin2dec converta
+-- corretamente uma lista de 0s e 1s (binário) em seu valor inteiro
+bin2dec :: [Int] -> Int
+bin2dec [] = undefined
+bin2dec bits = auxBin2Dec bits ((length bits)-1)
+
+auxBin2Dec :: [Int] -> Int -> Int
+auxBin2Dec _ (-1) = 0
+auxBin2Dec (x:xs) exp = x*(2^exp) + auxBin2Dec xs (exp-1)
+
+-- #4
+-- Mesmo que #3; não recursiva
+bin2dec' :: [Integer] -> Integer
+bin2dec' [] = undefined
+bin2dec' bits = foldl (\acc x -> acc*2 + x) 0 bits
