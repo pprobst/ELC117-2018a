643bd1d88b36b10c8bfe2fccfac2fbfb65ca32b2
pprobst <pprobst@insiberia.net>
2018-04-11 12:41:16 -0300

    1 e 2

--- t3/t3.pl
+++ t3/t3.pl
@@ -1,1 +1,19 @@
- 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% T3 - Programação lógica em Prolog %%%
+%%%        PEDRO PROBST MININI        %%%
+%%%            2018-04-11             %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% (1)
+% Defina um predicado zeroInit(L) que é verdadeiro se L for uma
+% lista que inicia com o número 0 (zero).
+
+zeroInit(L) :-
+    [H|_] = L,
+    H is 0.
+
+% (2)
+% Defina um predicado has5(L) que é verdadeiro se L for uma lista
+% de 5 elementos. Resolva este exercício sem usar um predicado aux.
+
+has5(L) :- L = [_,_,_,_,_].
