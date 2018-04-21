7eee019e4f253bc9bacb1ebfba12fcd33ca722bc
pprobst <pprobst@insiberia.net>
2018-04-21 18:02:04 -0300

    regras de posição

---
+++ t4/t4a.pl
@@ -0,0 +1,64 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% T4a -    CD Independente          %%%
+%%%        PEDRO PROBST MININI        %%%
+%%%            2018-04-21             %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Uma banda formada por alunos e alunas da escola está gravando um CD com       %
+% exatamente sete músicas distintas – S, T, V, W, X, Y e Z. Cada música ocupa   %
+% exatamente uma das sete faixas contidas no CD. Algumas das músicas são        %
+% sucessos antigos de rock; outras são composições da própria banda.            %
+%                                                                               % 
+% As seguintes restrições devem ser obedecidas:                                 %
+%                                                                               %
+% * S ocupa a quarta faixa do CD.                                               %
+%                                                                               %
+% * Tanto W como Y precedem S no CD (ou seja, W e Y estão numa faixa que é      %
+%   tocada antes de S no CD).                                                   %
+%                                                                               %
+% * T precede W no CD (ou seja, T está numa faixa que é tocada antes de W).     %
+%                                                                               %
+% * Um sucesso de rock ocupa a sexta faixa do CD.                               %
+%                                                                               %
+% * Cada sucesso de rock é imediatamente precedido no CD por uma composição     %
+%   da banda (ou seja, no CD cada sucesso de rock toca imediatamente após uma   %
+%   composição da banda).                                                       %
+%                                                                               %
+% * Z é um sucesso de rock.                                                     %
+%                                                                               %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% S ocupa a quarta faixa do CD
+regra1(CD) :- CD = [_, _, _, s, _, _, _].
+
+% W e Y precedem S
+regra2(CD) :-
+    nth0(Iw, CD, w),
+    nth0(Iy, CD, y),
+    nth0(Is, CD, s),
+    Iw < Is,
+    Iy < Is.
+
+% T precede W no CD
+regra3(CD) :-
+    nth0(It, CD, t),
+    nth0(Iw, CD, w),
+    It < Iw.
+
+% Permutação
+perm([],[]).
+perm(List, [H|Perm]) :- delete(H, List, Rest), perm(Rest, Perm).
+
+delete(X, [X|T], T).
+delete(X, [H|T], [H|NT]) :- delete(X, T, NT).
+
+% Regras combinadas
+faixas(CD) :-
+    CD = [_, _, _, _, _, _, _],
+    Musicas = [s, t, v, w, x, y, z],
+    perm(Musicas, CD),
+    regra1(CD),
+    regra2(CD),
+    regra3(CD).
