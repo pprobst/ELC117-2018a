f9cfad58f6aa779a0849f203e418a12453db72eb
pprobst <pprobst@insiberia.net>
2018-05-07 15:33:16 -0300

    usando @staticmethod

--- extras/OOP1_python/point.py
+++ extras/OOP1_python/point.py
@@ -14,8 +14,15 @@ class Point(object):
         dy = p2.y - self.y
         return sqrt(dx*dx + dy*dy)
 
+    @staticmethod
+    def distanciaStatic(p1, p2):
+        dx = p2.x - p1.x
+        dy = p2.y - p1.y
+        return sqrt(dx*dx + dy*dy)
+
 p1 = Point(4, 8)
 p2 = Point(6, 12)
 p1.desloca(2, 2)
 
 print(p1.distancia(p2))
+print(Point.distanciaStatic(p1, p2))
