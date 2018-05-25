07dae80908b767975e07d45f44506798c244df68
pprobst <pprobst@insiberia.net>
2018-05-25 16:52:42 -0300

    classe Grafo

---
+++ t5/Grafo.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Grafo {
+    public ArrayList<Vertice> vertices = new ArrayList<Vertice>();
+    public ArrayList<Aresta> arestas = new ArrayList<Aresta>();
+
+    // Construtor do Grafo
+    public Grafo(ArrayList<Vertice> vertices, ArrayList<Aresta> arestas) {
+        this.vertices = vertices;
+        this.arestas = arestas;
+    }
+
+    // Retorna o número de vértices
+    public int numVertices() {
+        return vertices.size();
+    }
+
+    // Retorna o número de arestas
+    public int numArestas() {
+        return arestas.size();
+    }
+
+    // Por enquanto isso...
+}
