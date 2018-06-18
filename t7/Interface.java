93f77f9adab40aa17c5a163d03b6cbc9047aa866
pprobst <pprobst@insiberia.net>
2018-06-18 09:24:32 -0300

    início

---
+++ t7/Interface.java
@@ -0,0 +1,100 @@
+package t7;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.layout.HBox;
+
+public class Interface extends Application {
+    private TableView<Onibus> table = new TableView<Onibus>();
+    private final ObservableList<Onibus> frota = 
+        FXCollections.observableArrayList();
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Frota de ônibus do Rio de Janeiro");
+
+        TableColumn datahCol = new TableColumn("Data/Hora");
+        datahCol.setMinWidth(100);
+        TableColumn ordemCol = new TableColumn("Ordem");
+        ordemCol.setMinWidth(30);
+        TableColumn linhaCol = new TableColumn("Linha");
+        linhaCol.setMinWidth(30);
+        TableColumn latCol = new TableColumn("Latitude");
+        latCol.setMinWidth(100);
+        TableColumn longCol = new TableColumn("Longitude");
+        longCol.setMinWidth(100);
+        TableColumn velCol = new TableColumn("Velocidade");
+        velCol.setMinWidth(100);
+
+        // Ajeitar mais tarde...
+        // Para evitar warnings
+        // TableView<DataEntry> table = new TableView<DataEntry>();
+        // TableColumn<DataEntry,String> datahCol = new TableColumn<DataEntry,String>("A");
+        // TableColumn<DataEntry,String> ordemCol = new TableColumn<DataEntry,String>("B");
+        //table.getColumns().add(datahCol);
+        //table.getColumns().add(ordemCol);
+
+        datahCol.setCellValueFactory(
+                new PropertyValueFactory<Onibus, String>("datah"));
+        datahCol.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        ordemCol.setCellValueFactory(
+                new PropertyValueFactory<Onibus, String>("ordem"));
+        ordemCol.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        linhaCol.setCellValueFactory(
+                new PropertyValueFactory<Onibus, String>("linha"));
+        linhaCol.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        latCol.setCellValueFactory(
+                new PropertyValueFactory<Onibus, String>("latitude"));
+        latCol.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        longCol.setCellValueFactory(
+                new PropertyValueFactory<Onibus, String>("longitude"));
+        longCol.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        velCol.setCellValueFactory(
+                new PropertyValueFactory<Onibus, String>("velocidade"));
+        velCol.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        table.setItems(frota);
+        table.getColumns().addAll(datahCol, ordemCol, linhaCol, latCol, longCol, velCol);
+
+        Button btnAtualiza = new Button("Atualizar");
+        btnAtualiza.setOnAction(e -> {
+            // lógica de atualização
+        });
+
+        HBox hbox = new HBox();
+        hbox.getChildren().addAll(btnAtualiza);
+        hbox.setSpacing(5);
+        hbox.setPadding(new Insets(15, 10, 15, 10));
+
+        VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 10, 10, 10));
+        vbox.getChildren().addAll(label, table, hbox);
+
+        stage.setScene(new Scene(vbox, 800, 600));
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ t7/Onibus.java
@@ -0,0 +1,99 @@
+package t7;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/* 
+ * Armazena e manipula as informações sobre um ônibus da frota da cidade do 
+ * Rio de Janeiro para mostrá-las nas tabelas da interface. 
+ *
+ */
+public class Onibus {
+    private final SimpleStringProperty datah;
+    private final SimpleStringProperty ordem;
+    private final SimpleStringProperty linha;
+    private final SimpleStringProperty latitude;
+    private final SimpleStringProperty longitude;
+    private final SimpleStringProperty velocidade;
+
+    public Onibus(String datah, String ordem, String linha, String latitude, 
+                  String longitude, String velocidade) {
+        this.datah = new SimpleStringProperty(datah);
+        this.ordem = new SimpleStringProperty(ordem);
+        this.linha = new SimpleStringProperty(linha);
+        this.latitude = new SimpleStringProperty(latitude);
+        this.longitude = new SimpleStringProperty(longitude);
+        this.velocidade = new SimpleStringProperty(velocidade);
+    }
+
+    public SimpleStringProperty datahProperty() {
+        return this.datah;
+    }
+
+    public String getDatah() {
+        return this.datah.get();
+    }
+
+    public void setDatah(String datah) {
+        this.datah.set(datah);
+    }
+
+    public SimpleStringProperty ordemProperty() {
+        return this.ordem;
+    }
+
+    public String getOrdem() {
+        return this.ordem.get();
+    }
+
+    public void setOrdem(String ordem) {
+        this.ordem.set(ordem);
+    }
+
+    public SimpleStringProperty linhaProperty() {
+        return this.linha;
+    }
+
+    public String getLinha() {
+        return this.linha.get();
+    }
+
+    public void setLinha(String linha) {
+        this.linha.set(linha);
+    }
+
+    public SimpleStringProperty latitudeProperty() {
+        return this.latitude;
+    }
+
+    public String getLatitude() {
+        return this.latitude.get();
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude.set(latitude);
+    }
+
+    public SimpleStringProperty longitudeProperty() {
+        return this.longitude;
+    }
+
+    public String getLongitude() {
+        return this.longitude.get();
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude.set(longitude);
+    }
+
+    public SimpleStringProperty velocidadeProperty() {
+        return velocidade;
+    }
+
+    public String getVelocidade() {
+        return this.velocidade.get();
+    }
+
+    public void setVelocidade(String velocidade) {
+        this.velocidade.set(velocidade);
+    }
+}
