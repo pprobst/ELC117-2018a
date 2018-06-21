a39bf9c24afca2118bcb4ac97aabaa1a43dbbc97
pprobst <pprobst@insiberia.net>
2018-06-21 13:52:34 -0300

    sem imports inúteis

--- t7/src/java/Control.java
+++ t7/src/java/Control.java
@@ -1,19 +1,12 @@
 package t7;
 
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
-import javafx.collections.transformation.SortedList;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
